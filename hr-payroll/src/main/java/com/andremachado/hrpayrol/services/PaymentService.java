fe243aa5a424f79dc1cd6cb223caa9e91e99c8f3
André do Rosário Cavalcante Machado <andre.machado@itriad.org.br>
2022-09-26 12:07:08 -0400

    restTemplate

--- hr-payroll/src/main/java/com/andremachado/hrpayrol/services/PaymentService.java
+++ hr-payroll/src/main/java/com/andremachado/hrpayrol/services/PaymentService.java
@@ -1,19 +1,25 @@
 package com.andremachado.hrpayrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.andremachado.hrpayrol.entities.Payment;
+import com.andremachado.hrpayrol.entities.Worker;
 
 
 @Service
 public class PaymentService {
 	
-//	@Autowired
-//	private WorkerFeignClient workerFeignClient;
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(long workerId, int days) {
-				
-//		Worker worker = workerFeignClient.findById(workerId).getBody();
-		return new Payment("Bob",200.0,days);
+		
+		Worker worker = restTemplate.getForObject(workerHost +"/workers/"+workerId, Worker.class);
+		return new Payment(worker.getName(),worker.getDailyInCome(),days);
 	}
 }
